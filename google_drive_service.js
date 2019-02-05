3c3f86e297fa766d69643cdc79d094a7046fd508
Sameer <sameer@ktds.co>
2019-02-05 19:45:09 +0530

    completed service account file upload in a directory

--- google_drive_service.js
+++ google_drive_service.js
@@ -2,12 +2,11 @@ const {google} = require('googleapis');
 const fs = require('fs');
 const key = require('./gdrive_service.json');
 
-const drive = google.drive('v3');
 const jwtClient = new google.auth.JWT(
   key.client_email,
   null,
   key.private_key,
-  ['https://www.googleapis.com/auth/drive'],
+  ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive.metadata.readonly'],
   null
 );
 
@@ -16,11 +15,12 @@ jwtClient.authorize((authErr) => {
     console.log(authErr);
     return;
   }
+  const drive = google.drive({version: 'v3', auth: jwtClient});
 
   // Make an authorized requests
 
   // List Drive files.
-  drive.files.list({ auth: jwtClient }, (listErr, resp) => {
+  drive.files.list((listErr, resp) => {
     if (listErr) {
       console.log(listErr);
       return;
@@ -30,4 +30,24 @@ jwtClient.authorize((authErr) => {
       console.log(`${file.name} (${file.mimeType})`);
     });
   });
+var fileMetadata = {
+'name': 'photo.jpg',
+parents: ['1oKs1GTSvOQCcPMaN43x9pCfjq3opbBel']
+};
+var media = {
+mimeType: 'image/jpeg',
+body: fs.createReadStream('./photo.jpg')
+};
+drive.files.create({
+resource: fileMetadata,
+media: media,
+fields: 'id'
+}, function (err, file) {
+if (err) {
+  // Handle error
+  console.error(err);
+} else {
+  console.log('File Id: ', file.id);
+}
+});
 });
